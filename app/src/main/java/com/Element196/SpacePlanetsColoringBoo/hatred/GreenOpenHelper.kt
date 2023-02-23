f3c9b836679f20964430592da34c5265a62bdba3
Asus <kirill.zhikhor15@gmail.com>
2023-02-23 11:18:01 +0200

    add lib 4

--- app/src/main/java/com/Element196/SpacePlanetsColoringBoo/hatred/GreenOpenHelper.kt
+++
@@ -1,9 +0,0 @@
-package com.Element196.SpacePlanetsColoringBoo.hatred
-
-import android.content.Context
-import com.element196.utils.element.DaoMaster
-
-class GreenOpenHelper(
-    context: Context,
-    name: String
-) : DaoMaster.OpenHelper(context, name)
